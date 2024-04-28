ffb5953b100442baf042871ad9c32045e8a27f14
Fahd <fahd31256@gmail.com>
2024-04-28 23:39:38 +0100

    fix: remove game

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-from game import Game
-import requests
+import game
 
-player_data = requests.get('https://127.0.0.1:2999/liveclientdata/playerlist', verify=False)
-event_data = requests.get('https://127.0.0.1:2999/liveclientdata/eventdata', verify=False)
-player_data = player_data.json()
-event_data = event_data.json()
+state = game.query_game_state()
 
-# print(event_data)
-# print(player_data)
-
-game = Game(player_data)
-game.update(player_data, event_data)
-print(game.get_state())
+print(state)
