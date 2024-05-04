3dcd1b75341babf0e0b78817224e9e936dd2aa41
Fahd <fahd31256@gmail.com>
2024-05-04 17:48:28 +0100

    feat: electron app

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import { query_game_state } from "./game.js";
-
-async function main() {
-	const [response, side] = await query_game_state();
-	console.log(response);
-	console.log(side);
-}
-
-main();
